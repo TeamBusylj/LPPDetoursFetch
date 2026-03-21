07e30fbb598b4a2c1f233654e08c8ccaa03abee0
TeamBusylj <88550318+TeamBusylj@users.noreply.github.com>
2026-03-21 15:18:01 +0100

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,5 +1,7 @@
-import fs from "fs/promises";
-import * as cheerio from "cheerio";
+import fs from 'fs/promises';
+import { existsSync, mkdirSync } from 'fs'; // Za ustvarjanje mape
+import crypto from 'crypto'; // Za generiranje edinstvenih imen slik
+import * as cheerio from 'cheerio';
 
 async function main() {
   const DETOUR_URL = "https://www.lpp.si/javni-prevoz/obvozi"; // Brez CORS proxyja!
@@ -43,6 +45,64 @@ async function parseDetours(html) {
       // Odstranimo nepotrebne elemente (isto kot tvoj prejšnji jQuery/DOMParser)
       $(".main--title").remove();
       $(".content__share--wrapper").remove();
+
+      // Ustvarimo mapo "slike", če še ne obstaja
+      if (!existsSync('slike')) {
+        mkdirSync('slike');
+      }
+
+      // Odstranimo nepotrebne elemente
+      $('.main--title').remove();
+      $('.content__share--wrapper').remove();
+      $('script, style, iframe, form').remove();
+      
+      // Pametna obdelava slik
+      $('img').each((i, el) => {
+        const src = $(el).attr('src');
+        
+        if (src) {
+          if (src.startsWith('data:image')) {
+            try {
+              // 1. Izluščimo tip slike in same podatke
+              const matches = src.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+              if (matches && matches.length === 3) {
+                const mimeType = matches[1]; // npr. image/jpeg
+                const base64Data = matches[2];
+                // Določimo končnico (.jpg, .png...)
+                const extension = mimeType.split('/')[1] === 'jpeg' ? 'jpg' : mimeType.split('/')[1];
+
+                // 2. Ustvarimo unikatno ime datoteke glede na njeno vsebino
+                const hash = crypto.createHash('md5').update(base64Data).digest('hex');
+                const filename = `${hash}.${extension}`;
+
+                // 3. Shranimo sliko fizično v mapo "slike"
+                const buffer = Buffer.from(base64Data, 'base64');
+                // Uporabimo sinhrono pisanje (samo za to podrobnost), ker smo znotraj cheerio zanke
+                import('fs').then(fsSync => fsSync.writeFileSync(`slike/${filename}`, buffer));
+
+                // 4. ZAMENJAJ TUKAJ: Vstavi svoj pravi GitHub username in ime repozitorija!
+                const githubRawUrl = `https://raw.githubusercontent.com/TVOJ_USERNAME/TVOJ_REPO/main/slike/${filename}`;
+                
+                // 5. V HTML-ju zamenjamo Base64 pošast s tem lepim, kratkim GitHub URL-jem
+                $(el).attr('src', githubRawUrl);
+              } else {
+                $(el).remove(); // Če je format čuden, jo raje brišemo
+              }
+            } catch (e) {
+              console.error("Napaka pri shranjevanju Base64 slike:", e);
+              $(el).remove();
+            }
+          } else if (src.startsWith('/')) {
+            // Normalne relativne slike
+            $(el).attr('src', 'https://www.lpp.si' + src);
+          }
+          
+          $(el).removeAttr('style').removeAttr('class').removeAttr('width').removeAttr('height');
+        }
+      });
+
+      // Vzamemo prečiščen HTML z novimi slikovnimi URL-ji
+      detailHtml = $('#content').html() || detailHtml;
 
       // Vzamemo samo vsebino in jo očistimo
       detailHtml = $("#content").html() || detailHtml;
