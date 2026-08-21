8e835730c3a02239045c426cf030c5f9cb34e237
TeamBusylj <88550318+TeamBusylj@users.noreply.github.com>
2026-08-21 13:50:05 +0200

    Update dujppNewsScraper.py

--- scripts/dujppNewsScraper.py
+++ scripts/dujppNewsScraper.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-BASE_URL = "https://www.dujpp.si/informacije-za-potnika.html"
+BASE_URL = "https://www.dujpp.si/Obvestila.html"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
